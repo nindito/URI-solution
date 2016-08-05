766fa9980c9990a462308e4bf6ceb18384952c20
Samsil Arefin <samsilarefin000@gmail.com>
2016-08-06 00:22:57 +0600

    Add files via upload

---
+++ 1002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+ 
+int main() {
+ 
+     double R,pi=3.14159;
+  cin>>R;
+  cout<<"A="<<fixed<<setprecision(4)<<pi*(R*R)<<endl;
+  
+  return 0;
+    
+}
